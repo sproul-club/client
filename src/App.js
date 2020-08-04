4982d00cf069adc63d969db1b7fe2fec97ecf794
Karen Tan <karentan@berkeley.edu>
2020-08-04 15:54:06 -0700

    sort of working modal, except when accessing from direct link (closing the modal will bring you back to blank page instead of catalog)

--- src/App.js
+++ src/App.js
@@ -15,38 +15,37 @@ import store from './store';
 import { Provider } from 'react-redux';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.previousLocation = this.props.location;
-  }
-  
+  previousLocation = this.props.location;
+
   componentWillUpdate() {
-    const { location } = this.props;
+    let { location } = this.props;
+
     if (!(location.state && location.state.modal)) {
-      this.previousLocation = this.props.location;
+      this.previousLocation = location;
     }
-  }  
+  }
+ 
   render() {
     const { location } = this.props;
 
-    const isModal = (
+    const isModal = !!(
       location.state &&
       location.state.modal &&
       this.previousLocation !== location
-    );
+    ); // not initial render
     
     console.log(location);
     return (
       <Provider store={store}>
         <Router>
-          <Switch location={isModal ? this.previousLocation : location}>
+          <Switch>
             <Route exact path="/" component={Landing} />
             <Route path="/catalog" component={Catalog} />
             <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/modal/:id" component={Modal}/>
+            <Route exact path="/modal/:id" component={Catalog}/>
             <Route>{'404'}</Route>
           </Switch>
-          {isModal ? <Route exact path="/modal/:id" component={Modal} /> : null }
+          <Route exact path="/modal/:id" component={Modal} />
         </Router>
       </Provider>
     );
--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -2,41 +2,41 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import './modal.css';
 
-class Modal extends Component {
-  constructor() {
-    super();
-    this.modalRef = React.createRef();
-  }
-
-  render() {
-    const { id } = this.props.match.params;
-
-    if (this.props.isModal) {
-      return (
-        <div
-          ref={this.modalRef}
-          className="modal-wrapper"
-          onClick={() => this.props.history.goBack()}
-        >
-          <div className="modal" onClick={e => e.stopPropagation()}>
-            <h1>{id}</h1>
-            <p>
-              hey the club modal view should render here
-            </p>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="no-modal-wrapper">
-          <h1>{id}</h1>
-          <p>
-            dis is the no modal wrapper view
-          </p>
-        </div>
-      );
-    }
-  }
-}
-
+const Modal = ({ match, history }) => {
+  const back = e => {
+    e.stopPropagation();
+    history.goBack();
+  };
+  return (
+    <div
+      onClick={back}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        background: "rgba(0, 0, 0, 0.15)"
+      }}
+    >
+      <div
+        className="modal"
+        style={{
+          position: "absolute",
+          background: "#fff",
+          top: 25,
+          left: "10%",
+          right: "10%",
+          padding: 15,
+          border: "2px solid #444"
+        }}
+      >
+        <h1>{match.params.id}</h1>
+        <button type="button" onClick={back}>
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
 export default withRouter(Modal);
