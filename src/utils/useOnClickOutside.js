b3c805705be0f419f4ba0d4cfef3232848ee2f72
mrclag <50685701+mrclag@users.noreply.github.com>
2020-08-06 13:15:44 -0700

    Add mobile responsive, reusable navbar

---
+++ src/utils/useOnClickOutside.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const useOnClickOutside = (ref, handler) => {
+  useEffect(() => {
+    const listener = event => {
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      handler(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [handler, ref]); // Empty array ensures that effect is only run on mount and unmount
+};
+
+export default useOnClickOutside;
