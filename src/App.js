9ed61b5d35753e4fc7a72e47141cf0f68c4c8ffe
amy-tong <amytongletters@gmail.com>
2020-08-10 16:26:28 -0700

    add signin page routing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { Landing } from './pages/Landing.js';
 import { ComingSoon } from './pages/ComingSoon.js';
 import Catalog from './pages/catalog.js';
 import { SignUp } from './pages/SignUp.js';
+import { SignIn } from './pages/SignIn.js';
 import Admin from './pages/admin/Admin.js';
 import Modal from './pages/Modal.js';
 import store from './store';
@@ -38,9 +39,9 @@ class App extends Component {
             <Route exact path="/" component={Landing} />
             <Route exact path="/comingsoon" component={ComingSoon} />
             <Route path="/admin" component={Admin} />
-            <Route path="/signin" component={ComingSoon} />
             <Route path="/catalog" component={Catalog} />
             <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/signin" component={SignIn} />
             <Route exact path="/club/:id" component={ComingSoon} />
             <Route>{'404'}</Route>
           </Switch>
