b009afb0c7cda191edf7336eff6ddd6a86eb19a9
amy-tong <amytongletters@gmail.com>
2020-08-10 16:26:49 -0700

    initial signin pages

---
+++ src/pages/SignIn.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './SignIn.css';
+import Navbar from '../layout/Navbar';
+import Footer from '../layout/Footer';
+
+const SignIn = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="signin">
+        <div className="content">
+
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export { SignIn };
