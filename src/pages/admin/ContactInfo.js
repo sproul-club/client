b4a59c9d5e022ba044c06ee04f970c1ef0e118cf
amy-tong <amytongletters@gmail.com>
2020-08-11 23:50:40 -0700

    changes

--- src/pages/admin/ContactInfo.js
+++ src/pages/admin/ContactInfo.js
@@ -7,6 +7,72 @@ const ContactInfo = () => {
       <div className="admin-text">
         Link your organization’s website, email and other social media platforms!
       </div>
+      <div className="formGroup">
+      <div className="formElement">
+          <p>Email Address <div>*</div></p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Website</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Linkedin</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Facebook</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Instagram</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Twitter</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Github</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Behance</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+        <div className="formElement">
+          <p>Medium</p>
+          <input
+              className="userInput"
+              type="text"
+          />
+        </div>
+      </div>
+      <button className="saveButton">Save changes</button>
     </div>
   );
 };
