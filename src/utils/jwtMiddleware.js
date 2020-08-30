a21bcbdf090085e12cadd8434f608396d4627c90
mrclag <50685701+mrclag@users.noreply.github.com>
2020-08-29 21:42:36 -0700

    fixed the access token issue, added a draft.js rich editor to play with

---
+++ src/utils/jwtMiddleware.js
@@ -0,0 +1,30 @@
+import {
+  // logout,
+  refreshToken,
+} from '../actions/auth';
+
+export default function authMiddleware({ dispatch, getState }) {
+  return (next) => async (action) => {
+    if (window.location.pathname.slice(0, 6) === '/admin') {
+      const token = localStorage.getItem('token');
+      const tokenExpire = localStorage.getItem('expiresAt');
+      // const refreshExpiresAt = localStorage.getItem('refreshExpiresAt');
+
+      if (token && isExpired(tokenExpire)) {
+        try {
+          await dispatch(refreshToken());
+          next(action);
+        } catch (e) {
+          // isExpired(refreshExpiresAt) && logout(history, false);
+          // console.log('called logout', e);
+        }
+      }
+    }
+    return next(action);
+  };
+}
+
+function isExpired(tokenExpire) {
+  let currentTime = new Date().getTime();
+  return currentTime > tokenExpire;
+}
