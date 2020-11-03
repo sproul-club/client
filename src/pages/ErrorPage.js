369b9ad265b746cad028ba100c405fccd65c59ef
Izzie Lau <isabellalau@berkeley.edu>
2020-11-02 20:33:51 -0800

    404 page :^)

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import './ErrorPage.css'
 
+import questionablebear from './assets/resetpwd1.png';
+
 const ErrorPage = () => {
   return (
     <div className="errorpage">
       <div className="content">
         <div className="imageContainer">
-          {/* Image here */}
+            <img
+              className="noresults-image"
+              src={questionablebear}
+              alt="a bear with many questions"
+            />
         </div>
         <div className="text">
           <h3>404 Error</h3>
