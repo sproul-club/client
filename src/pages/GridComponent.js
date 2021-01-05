3caa9c4e7d5cc4a3387951e9762dd2b3f36e9806
amy-tong <amytongletters@gmail.com>
2021-01-04 19:31:35 -0800

    remove image for now

--- src/pages/GridComponent.js
+++ src/pages/GridComponent.js
@@ -11,27 +11,13 @@ import uwubear from './assets/landingbears.svg';
 function GridComponent({ tagOptions, clubs, loading, formDetails, num_displayed }) {
 
   const [num_filtered_results, filteredClubs] = filterClubs(clubs, formDetails, tagOptions, num_displayed)
-  
+
   const GridList = filteredClubs.map((club, i) => <ClubCard key={i} tagOptions={tagOptions} club={club}/>);
 
   return (
     <div className="wrapper">
       <div className="num-results">
-        {num_filtered_results ? `Displaying ${num_filtered_results} Results` : loading ? '' : 
-            <div className="noresults-content">
-              <div className="noresults-text">
-              No Results Found
-              </div>
-              <div className="noresults-image-container">
-                  <img
-                    className="noresults-image"
-                    src={uwubear}
-                    alt="no results placeholder"
-                  />
-                  Try a different search!
-              </div>
-            </div>
-        }
+        {num_filtered_results ? `Displaying ${num_filtered_results} Results` : loading ? '' : 'No Results Found'}
       </div>
       <Grid justify='space-between' container className="card-grid">
         {GridList}
