61cb9c94996630c895583002492f883c48cdac82
mrclag <50685701+mrclag@users.noreply.github.com>
2021-01-13 21:21:08 -0800

    fixed line height issue on club card description

--- src/pages/ClubCard.js
+++ src/pages/ClubCard.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import './ClubCard.css'
 import logo from './assets/default_logo.jpg'
 import banner from './assets/default_banner.jpg'
 import { Link } from 'react-router-dom'
 
 const ClubCard = ({club, tagOptions, displayBanner}) => {
+
+  const [lines, setLines] = useState('one-line');
+
+  const measuredRef = useCallback(node => {
+    if (node !== null) {
+      const height = node.getBoundingClientRect().height
+      const lines = height <= 16 ? 1 : height <= 32 ? 'two-line' : 'three-line'
+      setLines(lines);
+    }
+  }, []);
 
   const {link_name, name, banner_url, logo_url, new_members, app_required } = club
 
@@ -23,8 +33,8 @@ const ClubCard = ({club, tagOptions, displayBanner}) => {
         <div className="clubcard-content-row1">
           <img src={logo_url || logo} alt="logo" className='clubcard-logo'/>
           <div className='clubcard-text'>
-            <div className="clubcard-title">{name}</div>
-            <div className="clubcard-description">We are an interactive platform where students can search for clubs, organizations, and communities relevant to their interests at UC Berkeley.</div>
+            <div className="clubcard-title" ref={measuredRef}>{name}</div>
+            <div className={lines + ' clubcard-description'}>We are an interactive platform where students can search for clubs, organizations, and communities relevant to their interests at UC Berkeley.</div>
           </div>
           <div className="clubcard-like">
             <i class="fas fa-heart"></i>
