535a3c81ab99ec136206de94e86b2df7064b1073
mrclag <50685701+mrclag@users.noreply.github.com>
2021-01-13 23:11:48 -0800

    made it so you can not highlight text on filters

--- src/pages/Catalog2.js
+++ src/pages/Catalog2.js
@@ -108,7 +108,7 @@ const Catalog2 = ({
           <img src={banner} alt="banner"/>
         </div>
         {/* ok so I was trying to put some of this styling in the filters class but it wasn't working so here it is in js */}
-        <div style={{position: 'sticky', top: '67px', paddingBottom: '10px', borderBottom: '1px solid #ccc'}} className='filters'>
+        <div style={{position: 'sticky', top: '67px', paddingBottom: '10px', borderBottom: '1px solid #ccc'}} className='filters noselect'>
 
           {/* Search Bar */}
           <div className="filter search-filter">
