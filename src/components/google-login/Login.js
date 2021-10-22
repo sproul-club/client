58433d61c030c6ac5a25e981b7ab20b162f1e037
Satiya <47483517+satiyak@users.noreply.github.com>
2021-10-22 10:18:52 -0700

    refresh token added

--- src/components/google-login/Login.js
+++ src/components/google-login/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import {GoogleLogin} from 'react-google-login';
+
+import { refreshTokenSetup } from './refreshTokenSetup';
 
 const clientId = '324245380972-5vcd95qj09o0m42fs5q5ogg48ba2j0po.apps.googleusercontent.com';
 
 function Login() {
     const onSuccess = (res) => {
         console.log('[Login success] currentUser:', res.profileObj);
+
+        refreshTokenSetup(res);
     };
     const onFailure = (res) => {
         console.log('[Login failed] res:', res);
---
+++ src/components/google-login/refreshTokenSetup.js
@@ -0,0 +1,14 @@
+export const refreshTokenSetup = (res) => {
+    
+    let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+
+    const refreshToken = async () => {
+        const newAuthRes = await res.reloadAuthResponse();
+        refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
+        console.log('newAuthRes:', newAuthRes);
+        console.log('new auth token:', newAuthRes.id_token);
+        setTimeout(refreshToken, refreshTiming);
+
+    };
+    setTimeout(refreshToken, refreshTiming);
+}
