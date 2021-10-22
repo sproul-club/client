7f243a4653ec616e4ec647eca6630e600f3e7e58
Allen Li <allenli75@berkeley.edu>
2021-10-22 14:49:51 -0700

    Whatever

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ import StudentSettings from './pages/student/StudentSettings.js';
 Moment.globalTimezone = 'America/Los_Angeles';
 
 let enableFeature = {
-  "dashboard": false,
+  "dashboard": true,
   "events": false,
   "new-club-profile": false
 }
