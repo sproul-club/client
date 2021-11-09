b6a351b6d47e096f0ac845c468378b2ed58d5bd0
karen tan <karentan@berkeley.edu>
2021-11-09 05:00:38 -0800

    entire event bar is clickable

--- src/components/calendar/CalendarEvent.js
+++ src/components/calendar/CalendarEvent.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Popover from '@material-ui/core/Popover';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import Moment from 'react-moment';
 import { simplestRangeFormat, START_DATETIME, END_DATETIME } from '../../utils/formatTimeAndDate';
 import './CalendarEvent.scss';
@@ -20,8 +18,8 @@ function CalendarEvent ({event}) {
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
     return (
-        <div className="rbc-event-content-inner">
-            <div >
+        <div>
+            <div className="rbc-event-content-inner" onClick={handleClick}>
                 <img src={event.icon.toString()}/>
                 {event.title.toString()}
             </div>
@@ -33,11 +31,11 @@ function CalendarEvent ({event}) {
                 onClose={handleClose}
                 anchorOrigin={{
                 vertical: 'bottom',
-                horizontal: 'center',
+                horizontal: 'left',
                 }}
                 transformOrigin={{
                 vertical: 'top',
-                horizontal: 'center',
+                horizontal: 'left',
                 }}
             >
                 <div className="popover-wrapper">
