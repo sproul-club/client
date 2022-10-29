dbb9ed14b61107f2cc4444f13a8066dcf62d7ccf
Ryan Huang <ryanhuang@berkeley.edu>
2022-10-29 14:32:33 -0700

    Migrated navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,17 @@ import { ThemeProvider } from "../contexts/Theme";
 import useTheme from "../contexts/Theme/useTheme";
 import "../styles/globals.scss";
 import { SignIn } from "../pages/clubLogin/ClubLogin.js";
+import Navbar from "../components/layout/navbar/Navbar.js";
+import "../components/layout/navbar/Navbar.scss";
 import "../pages/clubLogin/ClubLogin.scss";
 
 const AppWrapper = ({ Component, pageProps }: AppProps) => {
-  return <SignIn></SignIn>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <SignIn></SignIn>
+    </div>
+  );
 };
 
 // interface AppLogicProps {
