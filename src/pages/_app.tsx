cd55abac738b3f73a730d19230465a7af8f7c6c0
Amy Kwon <amygee1209@gmail.com>
2022-11-09 14:22:32 -0800

    card added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,9 +5,24 @@ import useTheme from "../contexts/Theme/useTheme";
 import "../styles/globals.scss";
 import { SignIn } from "../pages/clubLogin/ClubLogin.js";
 import "../pages/clubLogin/ClubLogin.scss";
+import { Card } from "./card/Card";
+import "../pages/card/Card.scss";
 
 const AppWrapper = ({ Component, pageProps }: AppProps) => {
-  return <SignIn></SignIn>;
+  return (
+    <>
+      <SignIn></SignIn>
+      <Card
+        clubName={"Web Development @ Berkeley"}
+        appReq={true}
+        open={true}
+        desc={
+          "Helping students to develop their coding & design skills in web dev."
+        }
+      />
+    </>
+  );
+  // return ;
 };
 
 // interface AppLogicProps {
