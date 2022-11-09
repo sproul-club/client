82ddc24defc7c4ca214b9930c163f1959ff13463
Ryan Huang <ryanhuang@berkeley.edu>
2022-11-08 20:40:48 -0800

    Added discover page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,18 @@ import { ThemeProvider } from "../contexts/Theme";
 import useTheme from "../contexts/Theme/useTheme";
 import "../styles/globals.scss";
 import { SignIn } from "../pages/clubLogin/ClubLogin.js";
+import Landing from "../pages/landing/Landing.js";
 import Navbar from "../components/layout/navbar/Navbar.js";
-import "../components/layout/navbar/Navbar.scss";
 import "../pages/clubLogin/ClubLogin.scss";
+import "../pages/landing/Landing.scss";
+import "../components/layout/navbar/Navbar.scss";
 
 const AppWrapper = ({ Component, pageProps }: AppProps) => {
   return (
     <div>
       <Navbar></Navbar>
-      <SignIn></SignIn>
+      {/* <SignIn></SignIn> */}
+      <Landing></Landing>
     </div>
   );
 };
