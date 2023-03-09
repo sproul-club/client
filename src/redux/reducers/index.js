52cf0028d8cb9a1adfc4ac89b6605ebb3b7b4824
Matt Clagett <mattrclagett@gmail.com>
2023-03-08 20:15:44 -0600

    Updates

--- src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from './auth';
-import profile from './profile';
-import catalog from './catalog'
-
-
-export default combineReducers({ auth, profile, catalog });
