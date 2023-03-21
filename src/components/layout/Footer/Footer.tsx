e530ad781dce3e5379bed89a659a765b853fb8e0
Genna Gams <ggams@berkeley.edu>
2023-03-20 17:57:25 -0700

    initial progress - working on spacing issue

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,5 +1,23 @@
+import Image from 'next/image';
+import heart from '../../../assets/icons/heart.svg';
+import instagram from '../../../assets/instagram.svg';
+import globe from '../../../assets/globe.svg';
+
+import styles from './Footer.module.scss';
+
 const Footer = () => {
-  return <div></div>;
+  return <div>
+    <div className={styles.loveMsg}>
+      <div>made with</div>
+      <Image src={heart} alt="heart" width={20} height={18} />
+      <div className={styles.flex}>by<div className={styles.lightBlue}> Web Development at Berkeley</div>!</div>
+    </div>
+    <div className={styles.icons}>
+      <Image src={instagram} alt="Instagram" width={20} height={20} />
+      <Image src={globe} alt="globe" width={20} height={20} />
+    </div>
+
+  </div >;
 };
 
 export default Footer;
