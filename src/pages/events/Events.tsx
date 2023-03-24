b0c0abbc7569be58dddce5fc877e63bd3ade5ab2
Kartik Aggarwal <kartikaggarwal@berkeley.edu>
2023-03-24 12:33:26 -0700

    Added button on Events Page

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,9 +1,14 @@
 import styles from './Events.module.scss';
+import Link from 'next/link';
 
 export default function Events() {
   return (
     <div>
-      <h1>Events</h1>
+      <h1 className="Events">
+        <button>
+          <Link href="/landing">Landing</Link>
+        </button>
+      </h1>
     </div>
   );
 }
