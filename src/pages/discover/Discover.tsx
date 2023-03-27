71a387c433ddf81e976367f3f397cba49853d23c
ishanthewizard <77704916+ishanthewizard@users.noreply.github.com>
2023-03-26 19:03:35 -0700

    Added some responsiveness and better centering

--- src/pages/discover/Discover.tsx
+++ src/pages/discover/Discover.tsx
@@ -6,7 +6,7 @@ export default function Discover() {
       <h1 className={styles.title}>Find your community at Berkeley!</h1>;
       <p className={styles.desc}>
         sproul.club helps you discover student clubs, organizations, and
-        communities on campus built by students, for students!
+        communities on campus -- built by students, for students!
       </p>
       <div>
         <input className={styles.searchbar} placeholder="Search Clubs" />
