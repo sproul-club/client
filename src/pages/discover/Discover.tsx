44b411c483f16ee139eaadcc284b695891f2c000
ishanthewizard <77704916+ishanthewizard@users.noreply.github.com>
2023-03-26 17:55:06 -0700

    Created barebone structre + added background

--- src/pages/discover/Discover.tsx
+++ src/pages/discover/Discover.tsx
@@ -1,5 +1,16 @@
 import styles from './Discover.module.scss';
 
 export default function Discover() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Find your community at Berkeley!</h1>;
+      <p className={styles.desc}>
+        sproul.club helps you discover student clubs, organizations, and
+        communities on campus built by students, for students!
+      </p>
+      <div>
+        <input className={styles.searchbar} placeholder="Search Clubs" />
+      </div>
+    </div>
+  );
 }
