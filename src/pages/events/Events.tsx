c524e234c33ce3511b73f246d340a0d880234827
Genna Gams <ggams@berkeley.edu>
2023-04-01 23:36:58 -0700

    initial progress - working search, possible Props implementation

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,14 +1,68 @@
 import styles from './Events.module.scss';
-import Link from 'next/link';
+import { HTMLProps, useState, useCallback } from 'react';
+import $ from 'jquery';
+import { Console } from 'console';
 
-export default function Events() {
+interface Tag {
+  name: string;
+}
+
+interface Link {
+  url: string;
+}
+
+interface Event {
+  title: string;
+  club: string;
+  date: string;
+  time: string;
+  location: string;
+  description: string;
+  tags: Tag[];
+  links: Link[];
+  image: string;
+}
+interface Props extends HTMLProps<HTMLDivElement> {
+  events: Event[];
+}
+
+
+export default function Events({ events }: Props) {
+  console.log(events)
+  // search functionality
+  const [query, setQuery] = useState('')
+  const onChange = useCallback((event) => {
+    const query = event.target.value
+    const formattedQuery = query.toLowerCase()
+    console.log(query)
+    $(`.${styles.event}`).each(function (i, item) {
+      if ($(item).find(`.${styles.event_name}`).text().toLowerCase().startsWith(formattedQuery)) {
+        $(item).css("display", "block")
+      } else {
+        $(item).css("display", "none")
+      }
+    });
+    setQuery(query)
+  }, [])
+
+
   return (
-    <div>
-      <h1 className="Events">
-        <button>
-          <Link href="/landing">Landing</Link>
-        </button>
-      </h1>
+    <div className={styles.container}>
+      <div className={styles.categories}>
+
+      </div>
+      <div className={styles.body}>
+        <div className={styles.search}>
+          <input
+            className={styles.search_box}
+            placeholder="Search for events"
+            type="text"
+            value={query}
+            onChange={onChange}
+          />
+
+        </div>
+      </div>
     </div>
   );
 }
