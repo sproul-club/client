dcb10ced02173958808cbae3fe7719aedf98b51b
lucerax <anic@Aniruddhs-MacBook-Pro.local>
2023-04-04 15:45:17 -0700

    Updated styling of club card and list, added icon links

--- src/pages/discover/ClubTab.tsx
+++ src/pages/discover/ClubTab.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import styles from './Discover.module.scss';
 import Club from '../../models/club/Club';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCheck,
+  faTimes,
+  faPencilAlt,
+  faHeart,
+} from '@fortawesome/free-solid-svg-icons';
 
 export type ClubTabProps = {
   club: Club;
@@ -29,22 +36,36 @@ export const ClubTab = ({
       <div className={styles.clubInfo}>
         <h3 className={styles.clubName}>{club.name}</h3>
         <div className={styles.clubTags}>
-          {club.categories.map((tag) => (
-            <span key={tag} className={styles.tag}>
-              {tag}
-            </span>
+          {club.categories.map((tag, index, arr) => (
+            <React.Fragment key={tag}>
+              <span className={styles.tag}>{tag}</span>
+              {index < arr.length - 1 && (
+                <span className={styles.separator}>·</span>
+              )}
+            </React.Fragment>
           ))}
         </div>
         <div className={styles.clubStatus}>
           {club.isApplicationOpen ? (
-            <div className={styles.openStatus}>Open</div>
+            <div className={styles.openStatus}>
+              <FontAwesomeIcon icon={faCheck} /> Open
+            </div>
           ) : (
-            <div className={styles.closedStatus}>Closed</div>
+            <div className={styles.closedStatus}>
+              <FontAwesomeIcon icon={faTimes} /> Closed
+            </div>
           )}
 
-          {club.isApplicationRequired && <span>Application Required</span>}
+          {club.isApplicationRequired && (
+            <span>
+              <FontAwesomeIcon icon={faPencilAlt} /> Application Required
+            </span>
+          )}
         </div>
       </div>
+      {selectedClub && club.id === selectedClub.id && (
+        <FontAwesomeIcon icon={faHeart} className={styles.heartIcon} />
+      )}
     </div>
   );
 };
